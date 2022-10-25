ddd1911af3dbd9153c081d3fc192237e62838835
argie <argeline0729@yahoo.com>
2022-10-24 23:23:19 -0400

    text and image is showing up on a page

---
+++ pages/index.js
@@ -0,0 +1,18 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
+
+//this is showing up on a page
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Pasta Restaurant</title>
+        <meta name="description" content="Best pasta in New York" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      homepage
+    <img src="/img/163-1638428_cook-png-transparent-background-.png"/>
+    </div>
+  )
+}
